f5aec7be981c58230b3e2368523b350d9eedb086
Villena Guillaume <contact@guillaumevillena.fr>
2020-12-07 19:30:48 +0100

    Added missing tests. Fixed travis binaries zip.

--- src/test/java/org/sqlite/AllTests.java
+++ src/test/java/org/sqlite/AllTests.java
@@ -7,6 +7,7 @@ import org.sqlite.util.OSInfoTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     BackupTest.class,
+    BusyHandlerTest.class,
     ConnectionTest.class,
     DBMetaDataTest.class,
     ErrorMessageTest.class,
@@ -15,25 +16,30 @@ import org.sqlite.util.OSInfoTest;
     FetchSizeTest.class,
     InsertQueryTest.class,
     JDBCTest.class,
-    OSInfoTest.class,
+    JSON1Test.class,
+    ListenerTest.class,
     PrepStmtTest.class,
+    ProgressHandlerTest.class,
     QueryTest.class,
     ReadUncommittedTest.class,
+    ResultSetTest.class,
     RSMetaDataTest.class,
     SavepointTest.class,
-    SQLiteDataSourceTest.class,
+    SQLiteConfigTest.class,
     SQLiteConnectionPoolDataSourceTest.class,
+    SQLiteDataSourceTest.class,
     SQLiteJDBCLoaderTest.class,
     StatementTest.class,
     TransactionTest.class,
+    TypeMapTest.class,
     UDFTest.class,
-    JSON1Test.class,
-    ProgressHandlerTest.class,
-    BusyHandlerTest.class,
+    OSInfoTest.class,
+
     // SQLiteMC related Tests
     SQLiteMCPragmaTest.class,
     SQLiteMCSQLInterfaceTest.class,
-    SQLiteMCURIInterfaceTest.class
+    SQLiteMCURIInterfaceTest.class,
+
 
 })
 public class AllTests {
